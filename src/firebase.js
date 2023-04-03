b0e50a4b0d1e340460f281bde0170ede014379be
Mustapha Ait-Hamou <mustaphaithamou@gmail.com>
2023-04-03 11:48:55 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAnsN7hPx8W6wptg-6r0oG4RI0zLQTITX4",
+    authDomain: "tamurt-video.firebaseapp.com",
+    projectId: "tamurt-video",
+    storageBucket: "tamurt-video.appspot.com",
+    messagingSenderId: "789658936483",
+    appId: "1:789658936483:web:2b2618f388c7c7134e8b16"
+  };
