5b7745e54011a4a14abda73752e88b9714187e3d
Mustapha Ait-Hamou <mustaphaithamou@gmail.com>
2023-04-05 17:21:08 +0200

    sign up screen added

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAnsN7hPx8W6wptg-6r0oG4RI0zLQTITX4",
     authDomain: "tamurt-video.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "789658936483",
     appId: "1:789658936483:web:2b2618f388c7c7134e8b16"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth }
+  export default db;
