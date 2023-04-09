5e4638c86e36a088f35f1f2f60e23182d9bec2ba
Mustapha Ait-Hamou <mustaphaithamou@gmail.com>
2023-04-09 14:07:23 +0200

    login/logout implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "./features/userSlice";
+import { login } from "./features/userSlice";
+
 
 function App() {
 
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
    const unsubscribe = auth.onAuthStateChanged(userAuth => {
       if (userAuth) {
-        //Logged in
-        console.log(userAuth);
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }));
       }
        else {
-        //Logged out
+        dispatch(logout);
        }
     });
 
@@ -30,6 +38,7 @@ function App() {
           <LoginScreen />
         ): (
         <Routes>
+          <Route path="/profile" element={<ProfileScreen />}/>
           <Route path="/" element={<HomeScreen />}/>
         </Routes>
         )}
